115c1ad9d4bb31430dce4d2db917cdf1c7c205f3
Ivan Sergeyev <i.i.sergeyev@gmail.com>
2017-02-20 20:53:21 +0300

    all info about the map is now stored in gamestate

--- src/gamestate.h
+++ src/gamestate.h
@@ -5,20 +5,52 @@
 #include "player.h"
 
 
+enum PLAYER_ACTIONS {ACT_SKIP = 0, ACT_MOVE, ACT_MELEE, ACT_SHOOT, ACT_BOMB};
+enum DIRECTIONS {DIR_NONE = -1, DIR_LEFT, DIR_UP, DIR_DOWN, DIR_RIHGT};
+enum OUTCOMES {OUT_SKIP = 0, OUT_PASS, OUT_WALL, OUT_WOUND, OUT_KILL,
+               OUT_BOMB_SUCCESS, OUT_BOMB_FAIL};
+
+
+enum MAP_TILES {TILE_UNDEFINED = -1,
+                TILE_EMPTY, TILE_HOLE, TILE_RIVER,
+                TILE_HOSPITAL, TILE_ARMORY, TILE_EXIT};
+
+
 class Gamestate {
  private:
-    Map *_map;
+    int _x_size, _y_size;
+    MAP_TILES **_tiles;
+    Wall **_walls;
+
+    int _num_treasures;
+    Treasure *_treasures;
+
     Player *_players;
 
- public:
-    Gamestate() {}
+    // todo : add other fields, e.g. list of exits
 
-    void save() {
-        // todo : save game
+ public:
+    Map() : _x_size(0), _y_size(0), _tiles(0) {}
+
+    Map(int x_size, int y_size) : _x_size(x_size), _y_size(y_size) {
+        // todo : empty map constructor
     }
 
-    void load() {
-        // todo : load game
+    void generate_map(int x_size, int y_size) {
+        // todo : add other parameters -- number of special tiles of each type,
+        //        number of treasures, etc.
+    }
+
+// todo : save
+// todo : load
+
+    void draw_ncurses() {
+        // todo : draw map
+        // todo : different draw methods: e.g. messages only
+    }
+
+    void attempt_move() {
+        // todo
     }
 };
 
