195b518f28ac16f5447e61f9675b613ef0706330
Ivan Sergeyev <i.i.sergeyev@gmail.com>
2017-02-23 13:36:55 +0300

    added next field to MapTile to implement rivers and holes

--- src/mechanics/headers/map_tile.h
+++ src/mechanics/headers/map_tile.h
@@ -6,27 +6,30 @@
 
 enum MAP_TILE_TYPES {
     MTT_UNDEFINED = -1,
-    MTT_EMPTY, MTT_HOLE,
+    MTT_FREE, MTT_HOLE,
     MTT_RIVER, MTT_RIVER_SOURCE, MTT_RIVER_MOUTH,
-    MTT_HOSPITAL, MTT_ARMORY, MTT_EXIT
+    MTT_MEDBAY, MTT_ARMORY, MTT_EXIT
 };
 
 
 class MapTile {
  private:
     MAP_TILE_TYPES _type;
-    MapWall walls[DIR_NUM_DIRECTIONS];
+    MapWall _walls[DIR_NUM_DIRECTIONS];
+    MapTile *_next;
 
  public:
     MapTile();
     explicit MapTile(MAP_TILE_TYPES type);
 
     MAP_TILE_TYPES get_type() const;
-
+    MapTile* get_next() const;
     bool has_wall(DIRECTIONS dir) const;
-    bool add_wall(MapWall wall);
 
+    void set_next(MapTile *next);
+    bool add_wall(MapWall wall);
     bool destroy_wall_in_direction(DIRECTIONS dir);
+
 };
 
 #endif  // SRC_MECHANICS_HEADERS_MAP_TILE_H_
--- src/mechanics/sources/map_tile.cpp
+++ src/mechanics/sources/map_tile.cpp
@@ -1,27 +1,37 @@
 #include "../headers/map_tile.h"
 
 
-MapTile::MapTile() : _type(MTT_UNDEFINED) {}
+MapTile::MapTile() :
+    _type(MTT_UNDEFINED), _next(nullptr) {}
 
-MapTile::MapTile(MAP_TILE_TYPES type) : _type(type) {}
+MapTile::MapTile(MAP_TILE_TYPES type) :
+    _type(type), _next(nullptr) {}
 
 MAP_TILE_TYPES MapTile::get_type() const {
     return _type;
 }
 
+MapTile* MapTile::get_next() const {
+    return _next;
+}
+
+void MapTile::set_next(MapTile *next) {
+    _next = next;
+}
+
 bool MapTile::has_wall(DIRECTIONS dir) const {
-    return dir != DIR_NONE && walls[dir].exists();
+    return dir != DIR_NONE && _walls[dir].exists();
 }
 
 bool MapTile::add_wall(MapWall wall) {
     DIRECTIONS dir = wall.get_direction();
     if (!has_wall(dir)) {
-        walls[dir] = wall;
+        _walls[dir] = wall;
         return true;
     }
     return false;
 }
 
 bool MapTile::destroy_wall_in_direction(DIRECTIONS dir) {
-    return walls[dir].destroy();
+    return _walls[dir].destroy();
 }
